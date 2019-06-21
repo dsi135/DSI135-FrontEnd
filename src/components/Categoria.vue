5ff75a0259325c5b0f8fdb918f486bf14e68d85f
DeadBryam <bryamfeik@gmail.com>
2019-06-21 03:48:44 -0600

    add/edit en productos y categorias

--- src/components/Categoria.vue
+++ src/components/Categoria.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <div >
-      <v-data-table :headers="headers" :items="categorias" class="">
+    <div>
+      <v-data-table :headers="headers" :items="categorias" class>
         <template v-slot:items="props">
-          <td class="text-xs-center">{{ props.item }}</td>
+          <td class="text-xs-center">{{ props.item.id }}</td>
+          <td class="text-xs-center">{{ props.item.nombre }}</td>
+          <td class="justify-center layout px-0">
+            <v-icon small class="mr-2" @click="editar(props.item)">edit</v-icon>
+          </td>
         </template>
       </v-data-table>
     </div>
@@ -11,20 +15,20 @@
       <div class="text-xs-center">
         <v-dialog v-model="nuevo" width="600">
           <template v-slot:activator="{ on }">
-            <v-btn flat dark v-on="on">NUEVA CATEGORIA</v-btn>
+            <v-btn flat dark @click="crear()">NUEVA CATEGORIA</v-btn>
           </template>
           <v-card>
-            <v-card-title primary-title class="headline light-blue lighten-1 center" >NUEVA CATEGORIA</v-card-title>
+            <v-card-title primary-title class="headline light-blue lighten-1 center">NUEVA CATEGORIA</v-card-title>
             <v-container>
               <v-layout row wrap>
-                <v-flex xs12 sm6 >
-                  <v-text-field label="Nombre de la Categoria" v-model="cat" required="true"></v-text-field>
+                <v-flex xs12 sm6>
+                  <v-text-field label="Nombre de la Categoria" v-model="cat.nombre" required="true"></v-text-field>
                 </v-flex>
               </v-layout>
             </v-container>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn flat @click="guardar(cat)">GUARDAR</v-btn>
+              <v-btn flat @click="ejecutar(cat.nombre)">GUARDAR</v-btn>
               <v-btn flat @click="nuevo = false">CANCELAR</v-btn>
             </v-card-actions>
           </v-card>
@@ -35,40 +39,77 @@
 </template>
 <script>
 import restMethods from "./../restMethods.js";
-import { log } from 'util';
+import { setTimeout } from 'timers';
 const rm = new restMethods();
 export default {
   data() {
     return {
       nuevo: false,
-      cat: null,
+      cat: {
+        id: null,
+        nombre: null
+      },
+      create: false,
       categorias: this.getcategorias(),
       headers: [
-        { text: "Categoria", value: "categorias" }
+        { text: "id", sortable: false, align: "center", value: "id" },
+        {
+          text: "Categoria",
+          sortable: false,
+          align: "center",
+          value: "categorias"
+        }
       ]
     };
   },
   methods: {
-    guardar(catgo) {
-     if(catgo!=null){
-      rm.postJson('categorias',{
-        id: null,
-        nombre: catgo
-      })
-      this.nuevo = false;
-      this.cat = null;
-     }
-    },
-    getcategorias(){
-      rm.getJson('categorias').
-        then(r=>{
-          let f= r.data.map(m=>{
-            return m.nombre;
-          });
-          this.categorias = f;
-        }).catch(e=>{
-          this.categorias = [""];
+    ejecutar(catgo) {
+      if (catgo != null && this.create && !this.cat.id) {
+        this.cat.nombre = catgo;
+        console.log(JSON.stringify(this.cat));
+        rm.postJson("categorias", {
+          id: this.cat.id,
+          nombre: this.cat.nombre
         });
+        this.nuevo = false;
+        this.cat.id = null;
+        this.cat.nombre = null;
+      } else if (catgo != null && !this.create && this.cat.id) {
+        console.log("edit" + JSON.stringify(this.cat));
+
+        this.cat.nombre = catgo;
+        rm.putJson("categorias/" + parseInt(this.cat.id), {
+          id: this.cat.id,
+          nombre: this.cat.nombre
+        });
+        this.nuevo = false;
+        this.cat.id = null;
+        this.cat.nombre = null;
+      }
+      
+      setTimeout(() => {
+        this.getcategorias();
+      }, 100);
+    },
+    getcategorias() {
+      rm.getJson("categorias")
+        .then(r => {
+          this.categorias = r.data;
+        })
+        .catch(e => {
+          this.categorias = [];
+        });
+    },
+    editar(item) {
+      this.nuevo = true;
+      this.cat = item;
+      this.create = false;
+    },
+    crear() {
+      this.cat.nombre = null;
+      this.nuevo = true;
+      this.cat.id = null;
+      this.create = true;
     }
   }
 };
--- src/components/Producto.vue
+++ src/components/Producto.vue
@@ -1,103 +1,199 @@
 <template>
-  <div>
+  <v-app>
     <div>
-      <v-data-table :headers="headers" :items="productos" class="elevation-1">
-        <template v-slot:items="props">
-          <td>{{ props.item.product }}</td>
-          <td class="text-xs-right">{{ props.item.precio }}</td>
-          <td class="text-xs-right">{{ props.item.categoria }}</td>
-        </template>
-      </v-data-table>
+      <v-card>
+        <v-card-title>
+          FILTRO
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="search"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="productos" :search="search" class="elevation-1">
+          <template v-slot:items="props">
+            <td class="text-xs-center">{{ props.item.id }}</td>
+            <td class="text-xs-center">{{ props.item.nombre }}</td>
+            <td class="text-xs-center">${{ props.item.precio }}</td>
+            <td class="text-xs-center">{{ props.item.categoria.nombre }}</td>
+            <td class="text-xs-center">{{ props.item.preparado }}</td>
+            <td class="justify-center layout px-0">
+              <v-icon small class="mr-2" @click="editReg(props.item)">edit</v-icon>
+            </td>
+          </template>
+          <template v-slot:no-results>
+            <v-alert
+              :value="true"
+              color="error"
+              icon="warning"
+            >Your search for "{{ search }}" found no results.</v-alert>
+          </template>
+        </v-data-table>
+      </v-card>
     </div>
 
     <template>
       <div class="text-xs-center">
         <v-dialog v-model="nuevo" width="600">
           <template v-slot:activator="{ on }">
-            <v-btn flat dark v-on="on">NUEVO PRODUCTO</v-btn>
+            <v-btn flat dark @click="crear()">NUEVO PRODUCTO</v-btn>
           </template>
           <v-card>
-            <v-card-title primary-title class="headline light-blue lighten-1 center" >NUEVO PRODUCTO</v-card-title>
+            <v-card-title primary-title class="headline light-blue lighten-1 center">NUEVO PRODUCTO</v-card-title>
             <v-container>
               <v-layout row wrap>
-                <v-flex xs12 sm6 >
+                <v-flex xs12 sm6>
                   <v-text-field label="Nombre del producto" v-model="produc"></v-text-field>
                 </v-flex>
               </v-layout>
               <v-layout row wrap>
-                <v-flex xs12 sm6 >
+                <v-flex xs12 sm6>
                   <v-text-field label="Precio del producto" v-model="preci"></v-text-field>
                 </v-flex>
               </v-layout>
               <v-layout row wrap>
                 <v-flex xs12 sm6 d-flex>
-                  <v-select :items="categorias" label="Categoria" v-model="cat"></v-select>
+                  <v-select
+                    :items="categorias"
+                    item-text="nombre"
+                    item-value="id"
+                    label="Categoria"
+                    v-model="cat"
+                  ></v-select>
+                </v-flex>
+              </v-layout>
+              <v-layout row wrap>
+                <v-flex xs12 sm6 d-flex>
+                  <v-select
+                    :items="[{id:true,val:'SI'},{id:false,val:'NO'}]"
+                    item-text="val"
+                    item-value="id"
+                    label="Preparado"
+                    v-model="prep"
+                  ></v-select>
                 </v-flex>
               </v-layout>
             </v-container>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn flat @click="guardar(produc,preci,cat)">GUARDAR</v-btn>
+              <v-btn flat @click="guardar(produc,preci,cat,prep)">GUARDAR</v-btn>
               <v-btn flat @click="nuevo = false">CANCELAR</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
       </div>
     </template>
-  </div>
+  </v-app>
 </template>
 <script>
+import restMethods from "./../restMethods.js";
+import { error, log } from "util";
+import { truncate } from 'fs';
+const rm = new restMethods();
 export default {
   name: "Producto",
   components: {},
   data() {
     return {
       nuevo: false,
+      idmagico: null,
       produc: null,
       preci: null,
       cat: null,
-      categorias: ["bebidas", "principal", "postres"],
-      productos: [
-        { product: "pepsi", precio: "1.00", categoria: "bebidas" },
-        { product: "coca cola", precio: "1.00", categoria: "bebidas" },
-        { product: "suprema", precio: "1.50", categoria: "bebidas" },
-        { product: "vodka", precio: "7.00", categoria: "bebidas" },
-        { product: "michelada", precio: "5.25", categoria: "bebidas" },
-        { product: "carne", precio: "5.00", categoria: "principal" },
-        { product: "pollo agridulce", precio: "6.75", categoria: "principal" },
-        { product: "sopa de patas", precio: "5.00", categoria: "principal" },
-        { product: "alitas", precio: "6.99", categoria: "principal" },
-        { product: "pizza", precio: "12.99", categoria: "principal" },
-        { product: "brownie", precio: "5.00", categoria: "postres" },
-        { product: "cheesecake", precio: "3.00", categoria: "postres" },
-        { product: "tres leches", precio: "2.00", categoria: "postres" },
-        { product: "crepa de nutella", precio: "4.50", categoria: "postres" }
-      ],
+      prep: null,
+      search: "",
+      edicion: false,
+      categorias: this.getCategorias(),
+      productos: this.getProductos(),
       headers: [
+        { text: "ID", align: "center", sortable: false, value: "id" },
+        { text: "Productos", align: "center", sortable: false, value: "prod" },
+        { text: "Precio", align: "center", value: "precio" },
         {
-          text: "Productos",
-          align: "left",
+          text: "Categoria",
+          align: "center",
           sortable: false,
-          value: "prod"
+          value: "categira"
         },
-        { text: "Precio", value: "precio" },
-        { text: "Categoria", value: "categira" }
+        { text: "Preparado", align: "center", value: "preparado" }
       ]
     };
   },
   methods: {
-    guardar(prducto, pre, catgo) {
+    guardar(prducto, pre, catgo, prep) {
       if (prducto != null && pre != null && catgo != null) {
-        let registro = { product: "", precio: "", categoria: "" };
-        registro.product = prducto;
-        registro.precio = pre;
-        registro.categoria = catgo;
-        this.productos.push(registro);
+        if (!this.editar) {
+          rm.postJson("productos", {
+            id: null,
+            categoria: {
+              id: catgo
+            },
+            nombre: prducto,
+            precio: pre,
+            preparado: prep
+          });
+        } else {
+          rm.putJson("productos/"+this.idmagico, {
+            id: this.idmagico,
+            categoria: {
+              id: catgo
+            },
+            nombre: prducto,
+            precio: pre,
+            preparado: prep
+          });
+        }
         this.nuevo = false;
         this.produc = null;
         this.preci = null;
         this.cat = null;
+        this.idmagico = null;
+        
+
+        setTimeout(() => {
+          this.getProductos();
+        }, 100);
       }
+    },
+    getProductos() {
+      rm.getJson("productos")
+        .then(r => {
+          this.productos = r.data;
+        })
+        .catch(e => {
+          console.error(e);
+          this.productos = [];
+        });
+    },
+    getCategorias() {
+      rm.getJson("categorias")
+        .then(r => {
+          this.categorias = r.data;
+        })
+        .catch(e => {
+          this.categorias = [];
+        });
+    },
+    crear() {
+      this.preci = null;
+      this.idmagico = null;
+      this.prep = null;
+      this.produc = null;
+      this.cat = null;
+      this.nuevo = true;
+      this.editar = false;
+    },
+    editReg(item) {
+      this.preci = item.precio;
+      this.idmagico = item.id;
+      this.prep = item.preparado;
+      this.produc = item.nombre;
+      this.cat = item.categoria.id;
+      this.nuevo = true;
+      this.editar = true;
     }
   }
 };
